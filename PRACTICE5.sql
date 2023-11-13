68bbf56020b7dd884f803ee795144d635c0b9531
thuytrangthuytrang <150219703+thuytrangthuytrang@users.noreply.github.com>
2023-11-13 14:10:25 +0700

    Update PRACTICE5.sql

--- PRACTICE5.sql
+++ PRACTICE5.sql
@@ -5,7 +5,37 @@ INNER JOIN country
   ON city.countrycode= country.code 
 GROUP BY country.continent;
 
--- EX2
+----- EX2
+SELECT 
+COUNT(texts.email_id)/COUNT(DISTINCT emails.email_id) AS confirm_rate
+FROM emails
+LEFT JOIN texts
+ON emails_id=texts_id  AND texts.signup_action = 'Confirmed';
+
+
+-----EX3
+
+SELECT age_breakdown.age_bucket,
+CASE 
+  WHEN activities.activity_type = 'open' THEN time_spent as time_open 
+  ELSE time_spent as time_send
+END 
+  THEN activities.time_spent/(activities.time_spent+) as open_perc
+  ELSE activities.time_spent/(activities.time_spent+) as send_perc
+END
+FROM activities
+LEFT JOIN age_breakdown
+ON activities.user_id=age_breakdown.user_id
+GROUP BY age_breakdown.age_bucket;
+
+----EX5
+SELECT mng.employee_id, emp.name,
+COUNT( emp.reports_to) as reports_count,
+AVG(emp.age) AS average_age
+FROM Employees AS emp
+JOIN Employees AS mng
+ON emp.employee_id = mng.employee_id 
+and emp.reports_count IS NOT NULL;
 
 -----EX6
 SELECT Products.product_name , SUM(Orders.unit) as unit 
