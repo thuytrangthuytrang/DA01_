68a430781e6c7bbbcc4e1d29ec295375a9ebe7ce
thuytrangthuytrang <150219703+thuytrangthuytrang@users.noreply.github.com>
2023-11-16 23:44:52 +0700

    Update PRACTICE6.sql

--- PRACTICE6.sql
+++ PRACTICE6.sql
@@ -27,78 +27,78 @@ LEFT JOIN page_likes
 WHERE page_likes.liked_date IS NULL
 ORDER BY  pages.page_id;
 
+--EX10
+WITH ect AS
+  (SELECT company_id, title, description, 
+  COUNT(job_id) as job  
+  FROM job_listings
+  GROUP BY company_id, title, description)
+SELECT COUNT(ect.company_id) AS duplicate_companies
+FROM ect 
+WHERE ect.job >1;
+
 --EX11
-/*tên người, tên phim*/
-WITH cte AS 
-(SELECT a.name, c.title, a.user_id 
-FROM Users AS a
-JOIN MovieRating AS b
-  ON a.user_id =b.user_id 
-JOIN Movies AS c
-  ON c.movie_id=b.movie_id
-WHERE EXTRACT(month FROM b.created_at)='2' 
-AND EXTRACT(year FROM b.created_at)='2020'),
+/* lượt phim nhiều nhất*/
+WITH cte1 AS
 
-  /*người bình chọn nhiều phim nhất*/
-cte2 AS
 (SELECT user_id, count(movie_id) as m
-FROM MovieRating 
-WHERE EXTRACT(month FROM created_at)='2' 
-AND EXTRACT(year FROM created_at)='2020'
-GROUP BY user_id),
+FROM MovieRating as c
+GROUP BY user_id
+Limit 1),
 
+/*tên phim đc xem nhiều nhất*/
+ cte2 AS
+ (SELECT name, COUNT(c.movie_id) as count
+ FROM  Users  as a
+JOIN MovieRating as c
+ON a.user_id = c.user_id 
+GROUP BY name
+HAVING count = (SELECT cte1.m FROM cte1)
+ORDER BY name
+LIMIT 1),
 
-/*phim có điêm bình chọn lớn nhất*/
-cte3 AS 
-(SELECT user_id,movie_id, AVG(rating) as n
-FROM MovieRating 
-WHERE EXTRACT(month FROM created_at)='2' 
-AND EXTRACT(year FROM created_at)='2020'
-GROUP BY movie_id)
+/* rating trung bình cao nhất*/
+cte3 AS
+(SELECT movie_id, AVG(rating ) as n
+FROM MovieRating as b
+GROUP BY movie_id
+ORDER BY n DESC
+Limit 1),
 
-SELECT  ect.name
-FROM ect
-JOIN ect2
-  ON ect.user_id=ect2.user_id
-JOIN ect3
-  ON ect3.user_id=ect.user_id
-WHERE MAX(cte2.m) AND MAX(cte3.n);
+/* phim có rating trung bình cao nhất*/
+ cte4 AS
+ (SELECT title , AVG(rating ) as d
+ FROM Movies  as e
+JOIN MovieRating as g
+ON e.movie_id = g.movie_id
+WHERE EXTRACT(month FROM g.created_at)='2' 
+AND EXTRACT(year FROM g.created_at)='2020'
+GROUP BY title
+HAVING d = (SELECT cte3.n FROM cte3)
+ORDER BY  title
+LIMIT 1
+)
 
+SELECT name as results
+FROM cte2
+UNION 
+SELECT title as results
+FROM cte4;
 
+--EX12
+WITH cte1 AS 
+(SELECT requester_id as id 
+FROM RequestAccepted
+UNION ALL 
+SELECT accepter_id as id
+FROM RequestAccepted)
 
+SELECT id, count(id) as num
+from cte1
+group by id
+order by num desc
+limit 1;
 
-WITH ect AS 
-
-(SELECT a.name , c.title, b.user_id
-FROM Users AS a
-JOIN MovieRating AS b
-  ON a.user_id =b.user_id 
-JOIN Movies AS c
-  ON c.movie_id=b.movie_id
-WHERE EXTRACT(month FROM b.created_at)='2' 
-AND EXTRACT(year FROM b.created_at)='2020'),
-
-ect2 AS
-(SELECT user_id, count(movie_id) as m
-FROM MovieRating
-WHERE EXTRACT(month FROM created_at)='2' 
-AND EXTRACT(year FROM created_at)='2020' 
-GROUP BY user_id),
-
-ect3 AS 
-(SELECT user_id, AVG(rating) as n 
-FROM MovieRating 
-WHERE EXTRACT(month FROM created_at)='2' 
-AND EXTRACT(year FROM created_at)='2020'
-GROUP BY movie_id),
-
-
-SELECT  ect.name
-FROM ect
-JOIN ect2
-  ON ect.user_id=ect2.user_id
-JOIN ect3
-  ON ect3.user_id=ect.user_id;
 
 
 
