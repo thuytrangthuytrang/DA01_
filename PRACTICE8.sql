ab0893ad36a97db0e47067e73e9ad90972a112a1
thuytrangthuytrang <150219703+thuytrangthuytrang@users.noreply.github.com>
2023-11-21 14:08:25 +0700

    Create PRACTICE8.sql

---
+++ PRACTICE8.sql
@@ -0,0 +1,41 @@
+--EX1--
+WITH cte AS
+(SELECT customer_id,order_date, customer_pref_delivery_date,
+COUNT(first_order) OVER(PARTITION BY customer_id ) as nnn
+FROM 
+(SELECT distinct customer_id, order_date,customer_pref_delivery_date,
+RANK() OVER(PARTITION BY customer_id ORDER BY  order_date  ) as first_order
+FROM Delivery) as cccc
+WHERE first_order=1),
+
+cte2 AS
+(SELECT customer_id,
+CASE 
+WHEN order_date =customer_pref_delivery_date THEN 1
+ELSE 0
+END AS immediate 
+FROM cte)
+
+SELECT round(100*sum(cte2.immediate)/count(cte.customer_id),2) as immediate_percentage
+FROM cte
+JOIN cte2 
+  ON cte.customer_id=cte2.customer_id;
+
+--EX2-- ĐỔI RA SỐ GIÂY Ở CASE WHEN 
+
+WITH cte AS
+(SELECT *,
+CASE
+WHEN  event_date =next_day-1 THEN 1
+ELSE 0
+END as nnn
+FROM 
+(SELECT player_id,event_date,
+LEAD(event_date) OVER(PARTITION BY player_id) as next_day 
+FROM Activity) AS aaa)
+
+SELECT ROUND(sum(nnn)/count(DISTINCT player_id),2) AS fraction 
+FROM cte;
+
+--EX3--
+
