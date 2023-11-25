1b52a6e9246da8787a53ac3a90e8146a9d373c84
thuytrangthuytrang <150219703+thuytrangthuytrang@users.noreply.github.com>
2023-11-26 00:09:33 +0700

    Update project2.sql

--- project2.sql
+++ project2.sql
@@ -22,6 +22,45 @@ where format_date('%Y-%m', created_at) between '2019-01' and '2022-04'
 group by 1
 order by 1
 
+
+/*3333333333333333*//
+
+  
+
+with cte as
+(
+select a.first_name, a.last_name,a.age,a.gender,
+      min(a.age) over(partition by a.gender) as min_age,
+      format_date('%Y-%m', b.created_at) as date
+from bigquery-public-data.thelook_ecommerce.users as a 
+join bigquery-public-data.thelook_ecommerce.orders as b 
+  on b.user_id=a.id
+where format_date('%Y-%m', b.created_at) between '2019/01' and '2022/04'
+),
+
+cte1 as
+(
+select a.first_name, a.last_name,a.age,a.gender,
+      max(a.age) over(partition by a.gender) as max_age,
+      format_date('%Y-%m', b.created_at) as date
+from bigquery-public-data.thelook_ecommerce.users as a 
+join bigquery-public-data.thelook_ecommerce.orders as b 
+  on b.user_id=a.id
+where format_date('%Y-%m', b.created_at) between '2019/01' and '2022/04'
+)
+
+select first_name, last_name,age,gender, 'youngest' as tag
+from cte
+where age= min_age
+
+union all
+
+select first_name, last_name,age,gender, 'oldest' as tag
+from cte1
+where age= max_age
+
+  
+
 /*********r4*/
 
   
@@ -64,5 +103,18 @@ join bigquery-public-data.thelook_ecommerce.products as c
 where format_date('%Y-%m-%d',a.created_at)between '2022-01-15' and '2022-04-15'
 group by 1,2   
 order by 1, sum(b.sale_price) desc
+
+
+/*****hgjh*/////
+
+select format_date('%Y-%m-%d',a.created_at) as dates,c.category as product_categories,
+sum(b.sale_price) over( partition by c.category order by format_date('%Y-%m-%d',a.created_at)) as revenue 
+from bigquery-public-data.thelook_ecommerce.orders as a
+join bigquery-public-data.thelook_ecommerce.order_items as b 
+  on a.order_id=b.id
+join bigquery-public-data.thelook_ecommerce.products as c 
+  on b.product_id=c.id
+where format_date('%Y-%m-%d',a.created_at)between '2022-01-15' and '2022-04-15'
+
 
 
